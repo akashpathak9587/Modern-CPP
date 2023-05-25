162bc0ed266aff8b5f8fdca163c1fbae3b6e11bc
akashpathak9587 <aakapa95@gmail.com>
2023-05-25 17:02:45 +0530

    different approact to check validity of string

--- checkStringValidity.cpp
+++ checkStringValidity.cpp
@@ -1,53 +1,52 @@
 #include <iostream>
-#include <set>
 using namespace std;
 
-int checkQwertyRow(char x)
+int checkRow(char c)
 {
-    set<char> first_row = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '_', '+'};
-    set<char> second_row = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'};
-    set<char> third_row = {'A', 'S', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
-    set<char> fourth_row = {'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
-    if (first_row.count(x) > 0)
+    vector<char> rows1 = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
+    vector<char> rows2 = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
+    vector<char> rows3 = {'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+    if (find(rows1.begin(), rows1.end(), c) != rows1.end())
     {
         return 1;
     }
-    else if (second_row.count(x) > 0)
+    if (find(rows2.begin(), rows2.end(), c) != rows2.end())
     {
         return 2;
     }
-    else if (third_row.count(x) > 0)
+    if (find(rows3.begin(), rows3.end(), c) != rows3.end())
     {
         return 3;
-    }
-    else if (fourth_row.count(x) > 0)
-    {
-        return 4;
     }
     return 0;
 }
 bool checkValidity(string str)
 {
-    char x = str[0];
-    int row = checkQwertyRow(x);
+    bool sameRow = true;
+    int startingNum = checkRow(tolower(str[0]));
     for (int i = 1; i < str.length(); i++)
     {
-        x = str[i];
-        if (row != checkQwertyRow(x))
+        if (startingNum != checkRow(tolower(str[i])))
         {
-            return false;
+            sameRow = false;
+            break;
         }
     }
-    return true;
+    return sameRow;
 }
 int main()
 {
-
-    string str = "hello";
-    if (checkValidity(str))
-        cout << "Yes" << endl;
-    else
-        cout << "No" << endl;
-
-    return 0;
+    vector<string> input = {"akash", "aman", "akdj"};
+    for (string str : input)
+    {
+        bool result = checkValidity(str);
+        if (result)
+        {
+            cout << "string character are in the same row" << endl;
+        }
+        else
+        {
+            cout << "string character are not in the same row" << endl;
+        }
+    }
 }
