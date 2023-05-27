3a89e2d0f2592f05986a4032f1878214215662e8
akashpathak9587 <aakapa95@gmail.com>
2023-05-27 10:11:35 +0530

    pure virtual distructor in c++

--- abstractClass.cpp
+++ abstractClass.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
-
+using namespace std;
 class AbstractClass
 {
 public:
     virtual void pureVirtualFunction() = 0; // Pure virtual function
-
+    virtual ~AbstractClass() = 0;           // pure virtual destructor
     void concreteFunction()
     {
-        std::cout << "This is a concrete function in the abstract class." << std::endl;
+        cout << "This is a concrete function in the abstract class." << endl;
     }
 };
+
+AbstractClass::~AbstractClass()
+{
+    cout << "base class destructor called" << endl;
+}
 
 class DerivedClass : public AbstractClass
 {
 public:
     void pureVirtualFunction() override
     {
-        std::cout << "Implementation of pure virtual function in the derived class." << std::endl;
+        cout << "Implementation of pure virtual function in the derived class." << endl;
+    }
+    ~DerivedClass()
+    {
+        cout << "Dervied class destructor called." << endl;
     }
 };
 
 int main()
 {
-    // AbstractClass abstractObj;  // Cannot instantiate an abstract class
+    // AbstractClass abstractObj; // Cannot instantiate an abstract class
     DerivedClass derivedObj;
     derivedObj.pureVirtualFunction(); // Calls the implementation in the derived class
     derivedObj.concreteFunction();    // Calls the concrete function from the abstract class
-
     return 0;
 }
